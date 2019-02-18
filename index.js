913b21b2fb5766e8cf44ae4d27c066e332d51da9
Ritsuki Goto <develop@mizucoffee.net>
2019-02-19 04:09:36 +0900

    [add] test program

---
+++ index.js
@@ -0,0 +1,33 @@
+const config = require('config')
+const fs = require('fs-extra')
+const Twitter = require('twitter')
+
+const client = new Twitter({
+  consumer_key: config.get('ck'),
+  consumer_secret: config.get('cs'),
+  access_token_key: config.get('tk'),
+  access_token_secret: config.get('ts')
+})
+
+const list = fs.readFileSync('list.txt', 'utf8').split('\n')
+  .filter(line => line !== '')
+  .filter(line => !line.startsWith('#'))
+
+  async function loop() {
+    const followers = await Promise.all(list.map(async screen_name => {
+      const user = await client.get('users/show', { screen_name: screen_name });
+      return user.followers_count
+    }))
+    console.log(followers.join(','))
+    fs.appendFile('output.csv', `${followers.join(',')}\n`)
+  }
+
+fs.pathExists('output.csv')
+  .then(exists => {
+    if(!exists) {
+      console.log(list.join(','))
+      fs.appendFile('output.csv', `${list.join(',')}\n`)
+    }
+    setInterval(loop, 1000 * 60 * 15)
+    loop()
+  })
